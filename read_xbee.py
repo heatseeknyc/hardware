5fc690817634b837e9aa110a9182252ef1f6ae52
Harold Cooper <hrldcpr@gmail.com>
2014-07-18 03:18:26 +0000

    doing our own buffering makes pylibftdi work great! removed 'serial' library support. outputting tsv, with an additional signature column.

--- read_xbee.py
+++ read_xbee.py
@@ -1,22 +1,30 @@
 import binascii
 import logging
 logging.basicConfig(level=logging.INFO)
-import os
 from struct import Struct
 import time
 
+import pylibftdi
+pylibftdi.driver.USB_PID_LIST.append(0x6015)
+
 SHORT = Struct('>H') # big endian unsigned short
 
-def read_sensor(xbee):
+def read(f, length):
+    s = b''
     while True:
-        frame_start = xbee.read(1)
-        if frame_start == b'\x7E': break
-        if frame_start: logging.error('expected frame start byte 0x7E but got {:02X}'.format(frame_start[0]))
+        s += f.read(length - len(s))
+        if len(s) == length: break
         time.sleep(0.01)
+    return s
 
-    length, = SHORT.unpack(xbee.read(2))
+def read_sensor(xbee):
+    frame_start, = read(xbee, 1)
+    if frame_start != 0x7E:
+        raise Exception('expected frame start byte 0x7E but got {:02X}'.format(frame_start))
 
-    frame = xbee.read(length + 1) # frame plus checksum byte
+    length, = SHORT.unpack(read(xbee, 2))
+
+    frame = read(xbee, length + 1) # frame plus checksum byte
 
     checksum = 0
     for byte in frame:
@@ -30,37 +38,18 @@ def read_sensor(xbee):
         sensor_global_id = frame[1:1+8]
         adc, = SHORT.unpack(frame[16:16+2])
 
-        voltage = adc / 0x3FF * 1.2
-        celsius = 25 + (voltage - 0.75) / 0.01
+        voltage = adc / 0x3FF * 1.2 # on Xbee, 0x3FF (highest value on a 10-bit ADC) corresponds to 1.2V
+        celsius = 25 + (voltage - 0.75) / 0.01 # on TMP36, 0.75V is 25C, and every 0.01V difference is 1C difference
         fahrenheit = celsius * (212 - 32) / 100 + 32
         
         sensor_global_id = binascii.hexlify(sensor_global_id).decode('ascii')
-        print('{},{},{}'.format(int(time.time()), sensor_global_id, fahrenheit))
 
+        signature = 'c0ffee' # TODO actually sign row using a secret key
 
-def main_serial():
-    import serial
+        print('{}\t{}\t{}\t{}'
+              .format(int(time.time()), sensor_global_id, int(fahrenheit), signature))
 
-    devs = os.listdir('/dev/')
-    usbserials = [dev for dev in devs if dev.startswith('tty.usbserial')]
-    if not usbserials:
-        logging.error('no /dev/tty.usbserial* device found; is the ftdi driver installed and the xbee usb dongle plugged in?')
-        return
-    logging.info('connecting to {}...'.format(usbserials[0]))
-    with serial.Serial('/dev/' + usbserials[0]) as xbee: # defaults to 9600/8N1
-        logging.info('...connected to {}'.format(xbee.name))
-        while True:
-            try:
-                read_sensor(xbee)
-            except Exception:
-                logging.exception("failed to read a packet")
-
-
-def main_libftdi():
-    import pylibftdi
-    import time
-    pylibftdi.driver.USB_PID_LIST.append(0x6015)
-
+def main():
     logging.info('attempting connection to ftdi usb...')
     with pylibftdi.Device() as xbee: # defaults to 9600/8N1
         logging.info('...connected')
@@ -69,7 +58,6 @@ def main_libftdi():
                 read_sensor(xbee)
             except Exception:
                 logging.exception("failed to read a packet")
-            
 
 if __name__ == '__main__':
-    main_libftdi()
+    main()
