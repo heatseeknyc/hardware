18f438c1e054453475ba9a7e853837670bef800f
wfjeff <wm.jeffries@gmail.com>
2014-07-18 13:34:15 -0400

    manually set uri.hostname and uri.port

--- pi/transmit.rb
+++ pi/transmit.rb
@@ -15,13 +15,14 @@ while on do
 
   readings.each do |reading|
     reading = reading.chop
-    time, sensor_name, temp, verification = reading.split('\t')
+    time, sensor_name, temp, verification = reading.split("\t")
 
     req = Net::HTTP::Post.new(uri, initheader = {'Content-Type' =>'application/json'})
     req.body = {reading: {sensor_name: sensor_name, temp: temp, time: time, verification: verification}}.to_json
-    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    res = Net::HTTP.start('heatseeknyc.com', 80) do |http|
       response = http.request(req)
       puts "Reading sent, response code = #{response.code}"
+      
       current_reading +=1 if response.code == "200"
     end
 
