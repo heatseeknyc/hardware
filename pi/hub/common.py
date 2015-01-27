ef38e83deead53d9f1057e044a5e492faa9c3cfc
Harold Cooper <harold@heatseeknyc.com>
2015-01-27 11:52:45 -0500

    useful @forever decorator, and no need for custom read() method now that we've stopped using libftdi

--- pi/hub/common.py
+++ pi/hub/common.py
@@ -1,14 +1,37 @@
+import functools
+import logging
 import sqlite3
 import time
 
+
+def forever(f):
+    @functools.wraps(f)
+    def g(*args, **kwargs):
+        while True:
+            try:
+                f(*args, **kwargs)
+            except Exception:
+                logging.exception('error, retrying...')
+                time.sleep(1)
+    return g
+
+
 class Database:
     def __init__(self):
-        self.db = sqlite3.connect('temperatures.db')
-        self.db.cursor().execute('create table if not exists temperatures'
-                                 ' (xbee, time, temperature)')
-        self.db.commit()
+        self.db = sqlite3.connect('heatseeknyc.db')
+        self.db.execute('create table if not exists temperatures (xbee, time, temperature)')
+        self.db.execute('create table if not exists transmitted as select 0 as temperature_id')
 
-    def insert(self, xbee, temperature):
-        self.db.cursor().execute('insert into temperatures values (?, ?, ?)',
-                               (xbee, round(time.time()), temperature))
-        self.db.commit()
+    def insert_temperature(self, xbee, temperature):
+        with self.db as db:
+            db.execute('insert into temperatures values (?, ?, ?)',
+                       (xbee, round(time.time()), temperature))
+
+    def get_untransmitted_temperatures(self):
+        with self.db as db:
+            (temperature_id,), = db.execute('select temperature_id from transmitted')
+            return db.execute('select rowid, * from temperatures where rowid > ?', temperature_id)
+
+    def set_transmitted_temperatures(self, temperature_id):
+        with self.db as db:
+            db.execute('update transmitted set temperature_id = ?', temperature_id)
--- pi/hub/receiver.py
+++ pi/hub/receiver.py
@@ -1,30 +1,21 @@
 import binascii
-import logging
-logging.basicConfig(level=logging.INFO)
-import serial
+import logging; logging.basicConfig(level=logging.INFO)
 from struct import Struct
-import time
 
-from . import database
+import serial
+
+from . import common
+
 
 SHORT = Struct('>H') # big endian unsigned short
 
-def read(f, length):
-    s = b''
-    while True:
-        s += f.read(length - len(s))
-        if len(s) == length: break
-        time.sleep(0.01)
-    return s
+def listen(xbee, db):
+    while xbee.read(1) != b'\x7E':
+        logging.warn('expected frame start byte 0x7E but got {:02X}'.format(frame_start))
 
-def read_sensor(xbee, db):
-    frame_start, = read(xbee, 1)
-    if frame_start != 0x7E:
-        raise Exception('expected frame start byte 0x7E but got {:02X}'.format(frame_start))
+    length, = SHORT.unpack(xbee.read(2))
 
-    length, = SHORT.unpack(read(xbee, 2))
-
-    frame = read(xbee, length + 1) # frame plus checksum byte
+    frame = xbee.read(length + 1) # frame plus checksum byte
 
     checksum = 0
     for byte in frame:
@@ -35,28 +26,28 @@ def read_sensor(xbee, db):
     if frame[0] == 0x92: # IO Data Sample RX
         if length != 18:
             raise Exception('expected length of 18 for 0x92 frame, but got {}'.format(length))
-        sensor_global_id = frame[1:1+8]
+        mac = frame[1:1+8]
         adc, = SHORT.unpack(frame[16:16+2])
+
+        mac = binascii.hexlify(mac).decode('ascii')
 
         voltage = adc / 0x3FF * 3.3 # on Xbee, 0x3FF (highest value on a 10-bit ADC) corresponds to VCC...ish
         celsius = (voltage - 0.5) / 0.01 # on MCP9700A, 0.5V is 0C, and every 0.01V difference is 1C difference
         fahrenheit = celsius * (212 - 32) / 100 + 32
 
-        logging.info('adc=0x{:x} voltage={:.2f} celsius={:.2f} fahrenheit={:.2f}'.format(adc, voltage, celsius, fahrenheit))
+        logging.info('mac={} adc=0x{:x} voltage={:.2f} celsius={:.2f} fahrenheit={:.2f}'.format(mac, adc, voltage, celsius, fahrenheit))
         
-        sensor_global_id = binascii.hexlify(sensor_global_id).decode('ascii')
+        db.insert_temperature(mac, round(fahrenheit, 2))
 
-        db.insert(sensor_global_id, round(fahrenheit, 2))
-
-
+@common.forever
 def main():
-    xbee = serial.Serial('/dev/ttyAMA0') # defaults to 9600/8N1
-    db = database.Database()
-    while True:
-        try:
-            read_sensor(xbee, db)
-        except Exception:
-            logging.exception("failed to read a packet")
+    db = common.Database()
+    logging.info('connected to database.')
+    with serial.Serial('/dev/ttyAMA0') as xbee:
+        logging.info('connected to xbee.')
+        while True:
+            listen(xbee, db)
 
 if __name__ == '__main__':
+    logging.info('starting...')
     main()
