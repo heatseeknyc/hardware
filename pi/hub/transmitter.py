2330eda2be2b5d1cbf386c9f9fa8cbd943da15bd
Harold Cooper <harold@heatseeknyc.com>
2015-01-28 18:49:38 -0500

    update database upon successful transmission

--- pi/hub/transmitter.py
+++ pi/hub/transmitter.py
@@ -12,8 +12,10 @@ READINGS_URI = 'http://hubs.heatseeknyc.com/readings'
 def transmit(db):
     for reading_id, cell_id, timestamp, temperature in db.get_untransmitted_readings():
         data = dict(hub=common.PI_ID, cell=cell_id, time=timestamp, temp=temperature)
-        logging.info('PATCHing {}'.format(data))
-        requests.patch(READINGS_URI, data)
+        logging.info(data)
+        response = requests.post(READINGS_URI, data)
+        if response.status_code != 200: raise Exception('bad response: {}'.format(response))
+        db.set_transmitted_readings(reading_id)
         time.sleep(1)
 
 @common.forever
