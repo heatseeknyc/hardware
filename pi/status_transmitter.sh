d8de2f0971daf51a2db8e0c43c5f2dc393517f28
Harold Cooper <harold@heatseeknyc.com>
2015-01-29 09:56:13 -0500

    new script posts ssh port every time it changes

---
+++ pi/status_transmitter.sh
@@ -0,0 +1,12 @@
+PI_ID=$(grep '^Serial\s*: ' /proc/cpuinfo | awk '{print $3}')
+
+PORT=""
+while true
+do NEWPORT=$(grep '^Allocated port ' /var/log/supervisor/autossh-stderr* | tail -1 | awk '{print $3}')
+    if [ "$NEWPORT" != "$PORT" ]
+	then if curl -d "hub=$PI_ID" -d "port=$NEWPORT" http://hubs.heatseeknyc.com/hubs
+	    then PORT="$NEWPORT"
+	fi
+    fi
+    sleep 60
+done
