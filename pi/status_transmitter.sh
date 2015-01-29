0fa23193f9da4241385e37a363d876a92e986e53
Harold Cooper <harold@heatseeknyc.com>
2015-01-29 15:37:23 -0500

    just use normal ssh, with a null packet every minute

--- pi/status_transmitter.sh
+++ pi/status_transmitter.sh
@@ -2,7 +2,7 @@ PI_ID=$(grep '^Serial\s*: ' /proc/cpuinfo | awk '{print $3}')
 
 PORT=""
 while true
-do NEWPORT=$(grep '^Allocated port ' /var/log/supervisor/autossh-stderr* | tail -1 | awk '{print $3}')
+do NEWPORT=$(grep '^Allocated port ' /var/log/supervisor/ssh-stderr* | tail -1 | awk '{print $3}')
     if [ "$NEWPORT" != "$PORT" ]
 	then if curl -d "hub=$PI_ID" -d "port=$NEWPORT" http://hubs.heatseeknyc.com/hubs
 	    then PORT="$NEWPORT"
